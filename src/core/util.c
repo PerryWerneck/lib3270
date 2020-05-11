c8245a591fa6efc50e206cd49dabd1df03360236
Perry Werneck <perry.werneck@gmail.com>
2020-05-10 23:47:43 -0300

    Version string should use timestamp value.

--- src/core/util.c
+++ src/core/util.c
@@ -367,7 +367,7 @@ LIB3270_EXPORT const char * lib3270_get_revision(void)
 
 LIB3270_EXPORT char * lib3270_get_version_info(void)
 {
-	return lib3270_strdup_printf("%s version %s-%d build %s",PACKAGE_NAME,PACKAGE_VERSION,PACKAGE_RELEASE,RPQ_REVISION);
+	return lib3270_strdup_printf("%s version %s-%d build %s",PACKAGE_NAME,PACKAGE_VERSION,PACKAGE_RELEASE,RPQ_TIMESTAMP_VALUE);
 }
 
 void lib3270_popup_an_errno(H3270 *hSession, int errn, const char *fmt, ...)

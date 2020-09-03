7393c9b2ec88b193cfab9af90928c82c0e3be067
Perry Werneck <perry.werneck@gmail.com>
2020-09-02 23:14:55 -0300

    Changing crl download property name.

--- src/core/properties/boolean.c
+++ src/core/properties/boolean.c
@@ -198,7 +198,7 @@
 		},
 
 		{
-			.name = "crlget",													//  Property name.
+			.name = "crl_download",												//  Property name.
 			.description = N_( "Non zero if the download of CRL is enabled" ),	//  Property description.
 			.get = lib3270_ssl_get_crl_download,								//  Get value.
 			.set = lib3270_ssl_set_crl_download									//  Set value.

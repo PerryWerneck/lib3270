abc9303d7388e3bda7c2db7940dc51f9aa7968b6
Perry Werneck <perry.werneck@gmail.com>
2023-03-25 12:57:28 -0300

    Debugging macos build.

--- mac/ci-build.sh
+++ mac/ci-build.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-echo "------------------ CURL"
-brew --prefix curl
+#export LIBCURL_LIBS="-L$(brew --prefix curl)/lib"
+#export LIBCURL_CFLAGS="-I$(brew --prefix curl)/include"
 
-echo "------------------ OpenSSL "
-brew --prefix openssl
-
+export PKG_CONFIG_PATH="$(brew --prefix curl)/lib/pkgconfig:$(brew --prefix openssl)/lib/pkgconfig"
 PROJECT_NAME=$(grep AC_INIT configure.ac | cut -d[ -f2 | cut -d] -f1)
 VERSION=$(grep AC_INIT configure.ac | cut -d[ -f3 | cut -d] -f1)
+
+echo "${PKG_CONFIG_PATH}"
+pkg-config --list-all
 
 ./autogen.sh --prefix="/${PROJECT_NAME}/${VERSION}"
 if [ "$?" != "0" ]; then
@@ -15,7 +16,6 @@ if [ "$?" != "0" ]; then
 fi
 
 make all
-./autogen.sh
 if [ "$?" != "0" ]; then
 	exit -1
 fi

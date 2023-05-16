fc95b410a051d203379ddfd8a1ed1e63f0d76494
Perry Werneck <perry.werneck@gmail.com>
2023-05-16 15:12:51 -0300

    Setting iconv path from brew in macos.

--- mac/ci-build.sh
+++ mac/ci-build.sh
@@ -8,7 +8,10 @@ VERSION=$(grep AC_INIT configure.ac | cut -d[ -f3 | cut -d] -f1)
 echo "${PKG_CONFIG_PATH}"
 pkg-config --list-all
 
-./autogen.sh --prefix="/${PROJECT_NAME}/${VERSION}"
+./autogen.sh \
+	--prefix="/${PROJECT_NAME}/${VERSION}" \
+	--with-libiconv-prefix=$(brew --prefix gettext)
+
 if [ "$?" != "0" ]; then
 	exit -1
 fi

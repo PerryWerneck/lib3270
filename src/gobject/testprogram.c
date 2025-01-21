0f246102b4198b3ef52a919f3b10c0166403ca09
Perry Werneck <perry.werneck@gmail.com>
2025-01-21 10:11:19 -0300

    Adding simple gobject test program.

---
+++ src/gobject/testprogram.c
@@ -0,0 +1,34 @@
+/* SPDX-License-Identifier: LGPL-3.0-or-later */
+
+/*
+ * Copyright (C) 2025 Perry Werneck <perry.werneck@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+ #include <config.h>
+ #include <glib.h>
+ #include <glib-object.h>
+ #include <glib/tn3270.h>
+
+ int main(int argc, char *argv[]) {
+
+	g_autoptr(TN3270Session) session = tn3270_session_new();
+
+
+
+	return 0;
+	
+ }
+

d0023cc7738f5a140ecf9a057b94ba7dd7289c1b
Perry Werneck <perry.werneck@gmail.com>
2025-02-22 09:11:10 -0300

    Better gobject cleanup.

--- src/gobject/testprogram.c
+++ src/gobject/testprogram.c
@@ -25,29 +25,84 @@
  #include <lib3270/glib.h>
  #include <lib3270/log.h>
  #include <lib3270/trace.h>
+ #include <getopt.h>
 
+ static GMainLoop *loop = NULL;
+
+ #ifdef HAVE_SIGNAL_H
+ #include <signal.h>
+ static void handle_signal(int sig) {
+	if(loop) {
+		g_main_loop_quit(loop);
+	}
+ }
+ #endif // HAVE_SIGNAL_H
 
  int main(int argc, char *argv[]) {
 
-	GMainLoop *loop = g_main_loop_new(g_main_context_default(), FALSE);
+	loop = g_main_loop_new(g_main_context_default(), FALSE);
 
 	g_autoptr(Tn3270Session) object = tn3270_session_new();
 
 	H3270 * hSession = tn3270_session_get_h3270(object);
 
-	lib3270_trace_open_file(hSession,"lib3270.trace");
-	lib3270_log_open_file(hSession,"lib3270.log",86400);
+	if(argc == 1) {
 
-	lib3270_log_write(hSession,"TEST","Testprogram %s starts (%s)",argv[0],LIB3270_STRINGIZE_VALUE_OF(PRODUCT_NAME));
-
-	lib3270_set_toggle(hSession,LIB3270_TOGGLE_DS_TRACE,1);
-	lib3270_set_toggle(hSession,LIB3270_TOGGLE_NETWORK_TRACE,1);
-	lib3270_set_toggle(hSession,LIB3270_TOGGLE_EVENT_TRACE,1);
-	lib3270_set_toggle(hSession,LIB3270_TOGGLE_SCREEN_TRACE,1);
-	lib3270_set_toggle(hSession,LIB3270_TOGGLE_SSL_TRACE,1);
+		lib3270_set_url(hSession,"tn3270://127.0.0.1:3270");
 	
-	lib3270_set_url(hSession,"tn3270://127.0.0.1:3270");
-	lib3270_connect(hSession,5);
+		lib3270_trace_open_file(hSession,"lib3270.trace");
+		lib3270_log_open_file(hSession,"lib3270.log",86400);
+	
+		lib3270_log_write(hSession,"TEST","Testprogram %s starts (%s)",argv[0],LIB3270_STRINGIZE_VALUE_OF(PRODUCT_NAME));
+	
+		lib3270_set_toggle(hSession,LIB3270_TOGGLE_DS_TRACE,1);
+		lib3270_set_toggle(hSession,LIB3270_TOGGLE_NETWORK_TRACE,1);
+		lib3270_set_toggle(hSession,LIB3270_TOGGLE_EVENT_TRACE,1);
+		lib3270_set_toggle(hSession,LIB3270_TOGGLE_SCREEN_TRACE,1);
+		lib3270_set_toggle(hSession,LIB3270_TOGGLE_SSL_TRACE,1);
+		
+		lib3270_set_toggle(hSession,LIB3270_TOGGLE_CONNECT_ON_STARTUP,1);
+		
+		lib3270_connect(hSession,5);
+	
+	} else {
+
+		int c;
+		while( (c = getopt(argc, argv, "u:t:l:ac")) != -1) {
+
+			switch(c) {
+			case 'u':
+				lib3270_set_url(hSession,optarg);
+				break;
+
+			case 't':
+				lib3270_trace_open_file(hSession,optarg ? optarg : "lib3270.trace");
+				break;
+
+			case 'l':
+				lib3270_log_open_file(hSession,optarg ? optarg : "lib3270.log",86400);
+				lib3270_log_write(hSession,"TEST","Testprogram %s starts (%s)",argv[0],LIB3270_STRINGIZE_VALUE_OF(PRODUCT_NAME));
+				break;
+
+			case 'a':
+				lib3270_set_toggle(hSession,LIB3270_TOGGLE_CONNECT_ON_STARTUP,1);
+				break;
+
+			case 'c':
+				lib3270_set_toggle(hSession,LIB3270_TOGGLE_CONNECT_ON_STARTUP,0);
+				lib3270_connect(hSession,5);
+				break;
+			}
+
+		}
+	}
+
+	#ifdef HAVE_SIGNAL_H
+		signal(SIGINT,handle_signal);
+		signal(SIGTERM,handle_signal);
+		signal(SIGQUIT,handle_signal);
+		signal(SIGHUP,handle_signal);
+	#endif // HAVE_SIGNAL_H
 
 	g_main_loop_run(loop);
 	g_main_loop_unref(loop);

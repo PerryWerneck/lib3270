0e6767bdea3ae310f9483d98d70442a11ac45b71
Perry Werneck <perry.werneck@gmail.com>
2025-04-13 00:09:01 -0300

    Debugging macos actions.

--- src/testprogram/testprogram.c
+++ src/testprogram/testprogram.c
@@ -25,13 +25,11 @@
  *
  */
 
+#include <config.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <getopt.h>
-#include <locale.h>
-#include <libintl.h>
-
 #include <internals.h>
 #include <lib3270.h>
 #include <lib3270/actions.h>
@@ -41,6 +39,11 @@
 #include <lib3270/properties.h>
 #include <lib3270/charset.h>
 #include <stdio.h>
+#include <locale.h>
+
+#ifdef HAVE_LIBINTL
+	#include <libintl.h>
+#endif // HAVE_LIBINTL
 
 #ifdef _WIN32
 	#include <lib3270/win32.h>
@@ -73,7 +76,7 @@ int main(int argc, char *argv[]) {
 	setlocale( LC_ALL, "" );
 #endif
 
-	textdomain("lib3270");
+	// textdomain("lib3270");
 
 	/*
 	{
